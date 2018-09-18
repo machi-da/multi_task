561a9b29425e5a9b404de56455aebcfff238dcc6
machida <t13304110@gunma-u.ac.jp>
2018-09-19 04:25:40 +0900

    update: w2vモデルの保存をtxtとbin両方で行う

--- word2vec.py
+++ word2vec.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+import logging
+from logging import getLogger
 from gensim.models import KeyedVectors, word2vec
 from tqdm import tqdm
 
@@ -10,15 +12,27 @@ http://www.cl.ecei.tohoku.ac.jp/~m-suzuki/jawiki_vector/
 
 
 class Word2Vec:
+    def __init__(self):
+        self.logger = getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('[%(asctime)s] %(message)s')
+
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)
+        sh.setFormatter(formatter)
+        self.logger.addHandler(sh)
+
     def train(self, text_file, size=256, window=5, sample=0.001, negative=5, hs=0):
-        print('Load text file')
+        logger = self.logger
+        logger.info('Load text_file')
         data = word2vec.LineSentence(text_file)
-        print('Start training')
+        logger.info('Start training')
         model = word2vec.Word2Vec(data, size=size, window=window, sample=sample, negative=negative, hs=hs)
-        print('Finish training')
+        logger.info('Finish training')
         model_name = '{}.w2v'.format(text_file)
-        model.wv.save_word2vec_format(model_name)
-        print('Save model: {}'.format(model_name))
+        model.wv.save_word2vec_format(model_name + '.txt')
+        model.wv.save_word2vec_format(model_name + '.bin', binary=True)
+        logger.info('Save model: {} and {}'.format(model_name + '.txt', model_name + '.bin'))
 
     def make_initialW(self, vocab, model_file='/home/lr/machida/entity_vector/entity_vector.model.bin'):
         """
@@ -44,13 +58,28 @@ class Word2Vec:
         return embedding_matrix, vector_size
 
 
-if __name__ == '__main__':
-    w2v = Word2Vec()
-    # vocab = {
-    #     'ハワイロア': 0
-    # }
-    # embedding_matrix, _ = w2v.make_initialW(vocab)
-    # print(embedding_matrix)
+def check(w2v):
+    vocab = {
+        'ハワイロア': 0
+    }
+    embedding_matrix, _ = w2v.make_initialW(vocab)
+    print(embedding_matrix)
 
+
+def train(w2v, text_file):
+    w2v.train(text_file)
+
+
+def most_similar(model, word):
+    embeddings_model = KeyedVectors.load_word2vec_format(model, binary=True)
+    print(embeddings_model.most_similar(word))
+
+
+if __name__ == '__main__':
     args = sys.argv
-    w2v.train(args[1])
+    w2v = Word2Vec()
+
+    if len(args) == 2:
+        train(w2v, args[1])
+    elif len(args) == 3:
+        most_similar(args[1], args[2])
